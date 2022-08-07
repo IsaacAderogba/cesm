19d5782ce01343f696473d58c0547dd0a1dfff6f
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-08-07 20:42:59 +0100

    Add test function on state

--- src/state/State.ts
+++ src/state/State.ts
@@ -1,8 +1,11 @@
+import { EPSILON } from "@utils/symbols";
+import { v4 as uuidv4 } from "uuid";
 export interface StateProps {
   isAccepting: boolean;
 }
 
 export class State {
+  id = uuidv4();
   isAccepting: StateProps["isAccepting"];
   private transitionsMap: Map<string, State[]> = new Map();
 
@@ -20,7 +23,28 @@ export class State {
     return this.transitionsMap.get(symbol) || [];
   }
 
-  test(string: string, visited = new Set()): boolean {
-    return true;
+  test(string: string, visited = new Set<State>()): boolean {
+    if (visited.has(this)) return false;
+    visited.add(this);
+
+    if (string.length === 0) {
+      if (this.isAccepting) return true;
+      for (const nextState of this.getTransition(EPSILON)) {
+        if (nextState.test("", visited)) return true;
+      }
+      return false;
+    }
+
+    const symbol = string[0];
+    const remainingString = string.slice(1);
+    for (const nextState of this.getTransition(symbol)) {
+      if (nextState.test(remainingString)) return true;
+    }
+
+    for (const nextState of this.getTransition(EPSILON)) {
+      if (nextState.test(string, visited)) return true;
+    }
+
+    return false;
   }
 }
