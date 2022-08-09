4ccc59f1d212c4c734f5147c3626d963c22f6c6c
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-08-09 07:18:04 +0100

    Add necessary exports

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export { and } from "./factories/and";
+export { char } from "./factories/char";
+export { eps } from "./factories/eps";
+export { opt } from "./factories/opt";
+export { or } from "./factories/or";
+export { plus } from "./factories/plus";
+export { rep } from "./factories/rep";
+
+export { Machine } from "./machine/Machine";
+export type { MachineProps } from "./machine/Machine";
+
+export { State } from "./state/State";
+export type { StateProps } from "./state/State";
+
+export { EPSILON, EPSILON_CLOSURE } from "./utils/symbols";
